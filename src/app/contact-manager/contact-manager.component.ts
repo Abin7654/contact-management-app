073e43effefb4b62197a2a9591c5a564be37dae4
Abin7654 <109271334+Abin7654@users.noreply.github.com>
2022-12-22 16:12:36 +0530

    contact-management-app

---
+++ src/app/contact-manager/contact-manager.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { MyContact } from 'src/models/myContacts';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-contact-manager',
+  templateUrl: './contact-manager.component.html',
+  styleUrls: ['./contact-manager.component.css']
+})
+export class ContactManagerComponent implements OnInit{
+
+  allContacts:MyContact[]=[]
+
+  searchKey:string=''
+  constructor(private api:ApiService){
+
+  }
+
+  ngOnInit(): void{
+    this.getAllContact()
+  }
+
+  //get all contact
+  getAllContact(){
+    this.api.getAllContacts().subscribe((data:any)=>{
+      console.log(data);
+      this.allContacts = data
+    })
+  }
+
+  //search
+  search(event:any){
+    console.log(event.target.value);
+    this.searchKey = event.target.value
+  }
+
+  deleteContact(contactId:any){
+    this.api.deleteContact(contactId)
+    .subscribe(
+      (data:any)=>{
+        //to get all contacts from Api
+        this.getAllContact()
+        //window.location.reload() - auto refresh
+      }
+    )
+  }
+
+}
